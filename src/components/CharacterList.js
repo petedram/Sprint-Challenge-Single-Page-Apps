47fbc72e6d8b2c0cf8694b6d3f4ef9ea9769214d
Pete James <petejames@petes-air.lan>
2019-12-14 11:36:25 -0800

    search feature complete

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -7,6 +7,8 @@ import {
 
 const CharacterList = props => {
   const [characters, setCharacters] = useState([]);
+
+
   useEffect(() => {
     const getCharacters = () => {
       axios
@@ -23,10 +25,59 @@ const CharacterList = props => {
     getCharacters();
   }, []);
 
+/// Search box:
+// array of names
+// state for searchTerm and searchResults
+// useEffect for searchTerm
+// handlechange 
+// return input onchange and value
+// return map of searchResults
+
+//array of name
+var listCharacterNames;
+listCharacterNames = characters.map(item => item.name);
+
+// state for searchTerm and searchResults
+const [searchTerm, setSearchTerm] = useState("");
+const [searchResults, setSearchResults] = useState(listCharacterNames);
+
+// useEffect for searchTerm
+useEffect(() => {}, [searchTerm]);
+
+// handlechange 
+  const handleChange = event => {
+    setSearchTerm(event.target.value);
+
+    const results = listCharacterNames.filter(character => {
+      return character.toLowerCase().includes(searchTerm.toLowerCase());
+    });
+
+    setSearchResults(results);
+  };
+
+
+  console.log("search term", searchTerm);
+  console.log('newNames', listCharacterNames);
+  console.log('searchresults', searchResults);
+
+
   return (
     <div className="character-list">
+      <form>
+        <label htmlFor="name">Search:</label>
+          <input
+            id="name"
+            type="text"
+            name="textfield"
+            placeholder="Search"
+            onChange={handleChange}
+            value={searchTerm}
+          />
+      </form>
       {characters.map(item => (
-        <CharacterDetails key={item.id} character={item} />
+
+        searchResults.includes(item.name, 0) && <CharacterDetails key={item.id} character={item} />
+        
       ))}
 
     </div>
@@ -40,11 +91,11 @@ function CharacterDetails({ character }) {
       <div className="contain">
       <Container className='character-card'>
         <Row xs='2'>
-            <Col>
+            <Col sm="12" md={{ size: 6, offset: 3 }}>
                 <Card>
                     <Link to={`/${id}`}>  
                     <CardHeader>Name: {name}</CardHeader>
-                    <CardImg width='300px' src={image} alt={name} />
+                    <CardImg  src={image} alt={name} />
                     </Link>
                     <CardBody><CardText>Status: {status}</CardText></CardBody>
                     <CardBody><CardText>Species: {species}</CardText></CardBody>
